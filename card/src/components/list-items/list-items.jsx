c873e5446443cae4ee9d5f4d32a1a77a6fb35817
fynaghk <n.fidan_15@mail.ru>
2021-03-09 17:37:26 +0400

    Done!

--- card/src/components/list-items/list-items.jsx
+++ card/src/components/list-items/list-items.jsx
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 import MinusButton from '../minus-button';
 import PlusButton from '../plus-button';
+import car1 from './img/car1.jpg';
+import car2 from './img/car2.jpg';
+import car3 from './img/car3.jpg';
 import './style.css';
 
 class ListItem extends Component {
+
     render() { 
         return (
             <div>
-                <li>text
+                <li><img src = {car1}/>
                 <PlusButton/><MinusButton/></li>
-                <li>text
+                <li><img src = {car2}/>
                 <PlusButton/><MinusButton/></li>
-                <li>text
+                <li><img src = {car3}/>
                 <PlusButton/><MinusButton/></li>
             </div>
         );
